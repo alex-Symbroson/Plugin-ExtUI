d0040d5eb8d7d8e51df3006c8d135dd53371b5ff
alex-symbroson <alex.symbroson@gmail.com>
2018-03-12 22:24:06 +0100

    added _ExtObj functions
    added most _ExtObj functions
    updated Switcher example
    changed minSDKVersion from 8 to 12

--- AndroidStudio/app/src/main/java/ru/starport/plugins/user/ExtUI.java
+++ AndroidStudio/app/src/main/java/ru/starport/plugins/user/ExtUI.java
@@ -13,6 +13,7 @@
 package ru.starport.plugins.user;
 
 //import android.annotation.TargetApi;
+
 import android.os.*;
 import android.util.*;
 import android.content.*;
@@ -88,9 +89,9 @@ public class ExtUI {
 				case "Plg":
 					return handlePlg(func[1], b);
 				case "Obj":
-					return handleObj(func[1], (View)obj, b);
+					return handleObj(func[1], (View) obj, b);
 				case "Swt":
-					return handleSwt(func[1], (Switch)obj, b);
+					return handleSwt(func[1], (Switch) obj, b);
 			}
 		} catch (Exception e) {
 			Log.e(TAG, "Plugin command '" + cmd + "' failed!", e);
@@ -110,25 +111,200 @@ public class ExtUI {
 	}
 
 	private String handleObj(String cmd, View obj, Bundle b) {
+
 		if (obj == null) return "";
 		Log.d(TAG, "handleObj -> " + cmd);
 
 		switch (cmd) {
-			case "SetEnabled":
-				obj.setEnabled(b.getBoolean("p1", false));
-				return "1";
+
+			case "BringToFront":
+				obj.bringToFront();
+				break;
+/*
+			case "ClearAnimation":
+				obj.clearAnimation();
+				break;
+*/
+			case "ClearFocus":
+				obj.clearFocus();
+				break;
+/*
+			case "DispatchTouchEvent":
+				return obj.dispatchTouchEvent(<MotionEvent>)? "1" : "0";
+*/
+			case "GetAlpha":
+				return Float.toString(obj.getAlpha());
+
+			case "GetBottom":
+				return Integer.toString(obj.getBottom());
+/*
+			case "GetDefaultSize":
+				return Integer.toString(View.getDefaultSize((int) Math.floor(b.getFloat("p1", 0.0f)), (int) Math.floor(b.getFloat("p2", 0.0f))));
+*/
+			case "GetHeight":
+				return Integer.toString(obj.getHeight());
+
+			case "GetId":
+				return Integer.toString(obj.getId());
+/*
+			case "GetLayerType":
+				return Integer.toString(obj.getLayerType());
+*/
+			case "GetLeft":
+				return Integer.toString(obj.getLeft());
+/*
+			case "GetMeasuredHeight":
+				return Integer.toString(obj.getMeasuredHeight());
+
+			case "GetMeasuredWidth":
+				return Integer.toString(obj.getMeasuredWidth());
+*/
+			case "GetRight":
+				return Integer.toString(obj.getRight());
+
+			case "GetRotation":
+				return Float.toString(obj.getRotation());
+
+			case "GetScaleX":
+				return Float.toString(obj.getScaleX());
+
+			case "GetScaleY":
+				return Float.toString(obj.getScaleY());
+
+			case "GetScrollX":
+				return Integer.toString(obj.getScrollX());
+
+			case "GetScrollY":
+				return Integer.toString(obj.getScrollY());
+
+			case "GetTop":
+				return Integer.toString(obj.getTop());
+
+			case "GetVisibility":
+				return Integer.toString(obj.getVisibility());
+
+			case "GetWidth":
+				return Integer.toString(obj.getWidth());
 
 			case "IsEnabled":
-				if(obj.isEnabled()) return "1";
-				else return "0";
+				return obj.isEnabled() ? "1" : "0";
 
-			case "SetOnTouch":
+			case "IsFocusable":
+				return obj.isFocusable() ? "1" : "0";
+
+			case "IsFocused":
+				return obj.isFocused() ? "1" : "0";
+
+			case "IsPressed":
+				return obj.isPressed() ? "1" : "0";
+
+			case "IsSelected":
+				return obj.isSelected() ? "1" : "0";
+
+			case "IsShown":
+				return obj.isShown() ? "1" : "0";
+/*
+			case "OnKeyDown":
+				return obj.onKeyDown((int) Math.floor(b.getFloat("p1", 0.0f)), <KeyEvent>)? "1" : "0";
+
+			case "OnKeyLongPress":
+				return obj.onKeyLongPress((int) Math.floor(b.getFloat("p1", 0.0f)), <KeyEvent>)? "1" : "0";
+
+			case "OnKeyUp":
+				return obj.onKeyUp((int) Math.floor(b.getFloat("p1", 0.0f)), <KeyEvent>)? "1" : "0";
+
+			case "OnTouchEvent":
+				return obj.onTouchEvent(<MotionEvent>)? "1" : "0";
+*/
+			case "PerformClick":
+				return obj.performClick() ? "1" : "0";
+
+			case "PerformHapticFeedback":
+				return obj.performHapticFeedback((int) Math.floor(b.getFloat("p1", 0.0f))) ? "1" : "0";
+
+			case "PlaySoundEffect":
+				obj.playSoundEffect(SoundEffectConstants.CLICK);
+				break;
+
+			case "ScrollBy":
+				obj.scrollBy((int) Math.floor(b.getFloat("p1", 0.0f)), (int) Math.floor(b.getFloat("p2", 0.0f)));
+				break;
+
+			case "ScrollTo":
+				obj.scrollTo((int) Math.floor(b.getFloat("p1", 0.0f)), (int) Math.floor(b.getFloat("p2", 0.0f)));
+				break;
+
+			case "SetAlpha":
+				obj.setAlpha(b.getFloat("p1", 0.0f));
+				break;
+/*
+		    case "SetAnimation":
+		        obj.setAnimation(<Animation>);
+		        break;
+
+		    case "SetBackground":
+		        obj.setBackground(<Drawable>);
+		        break;
+*/
+			case "SetBackgroundColor":
+				obj.setBackgroundColor((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetBottom":
+				obj.setBottom((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetClickable":
+				obj.setClickable(b.getBoolean("p1", false));
+				break;
+
+			case "SetEnabled":
+				obj.setEnabled(b.getBoolean("p1", false));
+				break;
+
+			case "SetFocusable":
+				obj.setFocusable(b.getBoolean("p1", false));
+				break;
+
+			case "SetHorizontalScrollBarEnabled":
+				obj.setHorizontalScrollBarEnabled(b.getBoolean("p1", false));
+				break;
+
+			case "SetLeft":
+				obj.setLeft((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+/*
+			case "SetLeftTopRightBottom":
+				obj.setLeftTopRightBottom((int) Math.floor(b.getFloat("p1", 0.0f)), (int) Math.floor(b.getFloat("p2", 0.0f)), (int) Math.floor(b.getFloat("p3", 0.0f)), (int) Math.floor(b.getFloat("p4", 0.0f))); break;
+
+			case "SetMinimumHeight":
+				obj.setMinimumHeight((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetMinimumWidth":
+				obj.setMinimumWidth((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetOnKey": {
 				final String callb = b.getString("p1");
-
-				if(callb != null && !callb.equals("")) {
+				if (callb != null && !callb.equals("")) {
+					obj.setOnKeyListener(new View.OnKeyListener() {
+						public void onKey(View v) {
+							if (!callb.equals("")) {
+								Bundle b = new Bundle();
+								b.putString("cmd", callb);
+								CallScript(b);
+							}
+						}
+					});
+				}
+			}
+			break;
+*/
+			case "SetOnClick": {
+				final String callb = b.getString("p1");
+				if (callb != null && !callb.equals("")) {
 					obj.setOnClickListener(new View.OnClickListener() {
-
-						//@Override
 						public void onClick(View v) {
 							if (!callb.equals("")) {
 								Bundle b = new Bundle();
@@ -138,6 +314,134 @@ public class ExtUI {
 						}
 					});
 				}
+			}
+			break;
+
+			case "SetOnLongClick": {
+				final String callb = b.getString("p1");
+				if (callb != null && !callb.equals("")) {
+					obj.setOnLongClickListener(new View.OnLongClickListener() {
+						public boolean onLongClick(View v) {
+							if (!callb.equals("")) {
+								Bundle b = new Bundle();
+								b.putString("cmd", callb);
+								CallScript(b);
+							}
+							return false;
+						}
+					});
+				}
+			}
+			break;
+/*
+			case "SetOnScrollChange": {
+				final String callb = b.getString("p1");
+				if (callb != null && !callb.equals("")) {
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+						obj.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+							public void onScrollChange(View v) {
+								if (!callb.equals("")) {
+									Bundle b = new Bundle();
+									b.putString("cmd", callb);
+									CallScript(b);
+								}
+							}
+						});
+					}
+				}
+			}
+			break;
+*/
+			case "SetPadding":
+				obj.setPadding((int) Math.floor(b.getFloat("p1", 0.0f)), (int) Math.floor(b.getFloat("p2", 0.0f)), (int) Math.floor(b.getFloat("p3", 0.0f)), (int) Math.floor(b.getFloat("p4", 0.0f)));
+				break;
+
+			case "SetRight":
+				obj.setRight((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetRotation":
+				obj.setRotation(b.getFloat("p1", 0.0f));
+				break;
+
+			case "SetScaleX":
+				obj.setScaleX(b.getFloat("p1", 0.0f));
+				break;
+
+			case "SetScaleY":
+				obj.setScaleY(b.getFloat("p1", 0.0f));
+				break;
+
+			case "SetScrollBarSize":
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+					obj.setScrollBarSize((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetScrollBarStyle":
+
+				switch (b.getString("p1", "Hide").toLowerCase()) {
+
+					case "insideinset":
+						obj.setScrollBarStyle(View.SCROLLBARS_INSIDE_INSET);
+						break;
+
+					case "insideoverlay":
+						obj.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+						break;
+
+					case "outsideinset":
+						obj.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_INSET);
+						break;
+
+					case "outsideoverlay":
+						obj.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+						break;
+				}
+				break;
+
+			case "SetScrollX":
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+					obj.setScrollX((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetScrollY":
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+					obj.setScrollY((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetSelected":
+				obj.setSelected(b.getBoolean("p1", false));
+				break;
+
+			case "SetTop":
+				obj.setTop((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetVerticalScrollBarEnabled":
+				obj.setVerticalScrollBarEnabled(b.getBoolean("p1", false));
+				break;
+
+			case "SetVerticalScrollbarPosition":
+				obj.setVerticalScrollbarPosition((int) Math.floor(b.getFloat("p1", 0.0f)));
+				break;
+
+			case "SetVisibility":
+
+				switch (b.getString("p1", "Hide").toLowerCase()) {
+
+					case "show":
+						obj.setVisibility(View.VISIBLE);
+						break;
+
+					case "hide":
+						obj.setVisibility(View.INVISIBLE);
+						break;
+
+					case "gone":
+						obj.setVisibility(View.GONE);
+						break;
+				}
+				break;
 		}
 		return "";
 	}
@@ -148,6 +452,7 @@ public class ExtUI {
 		Log.d(TAG, "handleSwt -> " + cmd);
 
 		switch (cmd) {
+
 			case "SetChecked":
 				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
 					obj.setChecked(b.getBoolean("p1", false));
@@ -155,7 +460,7 @@ public class ExtUI {
 				} else return "0";
 
 			case "GetChecked":
-				if(obj.isChecked()) return "1";
+				if (obj.isChecked()) return "1";
 				else return "0";
 		}
 		return "0";
--- Examples/Switcher/Switcher.js
+++ Examples/Switcher/Switcher.js
@@ -6,17 +6,84 @@ function OnStart() {
 	var lay = app.CreateLayout( "linear", "VCenter,FillXY" );	
 	
 	swt = plg.CreateSwitch();
-	swt.SetOnTouch(swt_OnTouch);
 	lay.AddChild( swt );
 	
 	app.AddLayout( lay );
 	
-	alert("Version: " + plg.GetVersion());
-	alert("SetEnabled: " + (swt.SetEnabled(true)? "" : "un") + "successful");
-	alert("SetChecked: " + (swt.SetChecked(true)? "" : "un") + "successful");
-	alert("GetChecked: " + swt.GetChecked());
+	var res = "";
+	function test(o, f, p) { 
+	    p = p || [];
+	    res += (o && o.GetType? o.GetType() + "." : "") + f + "(" + p.join(", ") + "):\n";
+	    try { res += JSON.stringify(o[f].apply(o,p)) + "\n\n"; }
+	    catch(e) { res += e; }
+	}
+	
+	test(plg, "GetVersion" );
+	
+	test(swt, "BringToFront", [] );
+	test(swt, "ClearFocus", [] );
+	test(swt, "GetAlpha", [] );
+	test(swt, "GetBottom", [] );
+	test(swt, "GetHeight", [] );
+	test(swt, "GetId", [] );
+	test(swt, "GetLeft", [] );
+	test(swt, "GetRight", [] );
+	test(swt, "GetRotation", [] );
+	test(swt, "GetScaleX", [] );
+	test(swt, "GetScaleY", [] );
+	test(swt, "GetScrollX", [] );
+	test(swt, "GetScrollY", [] );
+	test(swt, "GetTop", [] );
+	test(swt, "GetVisibility", [] );
+	test(swt, "GetWidth", [] );
+	test(swt, "IsEnabled", [] );
+	test(swt, "IsFocusable", [] );
+	test(swt, "IsFocused", [] );
+	test(swt, "IsPressed", [] );
+	test(swt, "IsSelected", [] );
+	test(swt, "IsShown", [] );
+	test(swt, "PerformClick", [] );
+    //test(swt, "PerformHapticFeedback", [1] ); //ne
+    //test(swt, "PlaySoundEffect", [] ); //ne
+    	//test(swt, "ScrollBy", [intg, intg] );
+    	//test(swt, "ScrollTo", [intg, intg] );
+	test(swt, "SetAlpha", [0.6] );
+	    //test(swt, "SetBackgroundColor", [3] ); //?
+	//test(swt, "SetBottom", [.9] ); //ne
+	test(swt, "SetClickable", [true] );
+	test(swt, "SetEnabled", [true] );
+	test(swt, "SetFocusable", [true] );
+	test(swt, "SetHorizontalScrollBarEnabled", [true] );
+	//test(swt, "SetLeft", [0.3] ); //ne
+	test(swt, "SetOnClick", [swt_OnClick] );
+	test(swt, "SetOnLongClick", [swt_OnLongClick] );
+	//test(swt, "SetPadding", [flot, flot, flot, flot] );
+	//test(swt, "SetRight", [0.3] ); //ne
+	test(swt, "SetRotation", [45] );
+	test(swt, "SetScaleX", [1.4] );
+	test(swt, "SetScaleY", [2] );
+        //test(swt, "SetScrollBarSize", [500] ); //ne
+    	//test(swt, "SetScrollBarStyle", [style] ); //ne
+        //test(swt, "SetScrollX", [0.5] ); //ne
+    	//test(swt, "SetScrollY", [flot] ); //ne
+	test(swt, "SetSelected", [true] );
+    //test(swt, "SetTop", [0.2] ); //ne
+	    //test(swt, "SetVerticalScrollBarEnabled", [true] ); //ne
+    	//test(swt, "SetVerticalScrollbarPosition", [intg] ); //ne
+	test(swt, "SetVisibility", ["Show"] );
+	
+	alert(res);
+	
+	setInterval("swt.PerformClick()", 3000);
+	
+	rotation = 45;
+	setInterval("swt.SetRotation(rotation+=45)", 1000);
 }
 
-function swt_OnTouch() {
+function swt_OnClick() {
     app.ShowPopup("switch touched!\nEnabled: " + swt.GetChecked());
+}
+
+function swt_OnLongClick() {
+    app.ShowPopup("switch long touched!\nEnabled: " + swt.GetChecked());
 }
